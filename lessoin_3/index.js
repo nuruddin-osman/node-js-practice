a91b18127e06edefdc814bccec82c941477f76d2
mnosumon <mdnuruddin378350@gmail.com>
2025-08-10 23:37:57 +0600

    os madule and path module practice done

---
+++ lessoin_3/index.js
@@ -0,0 +1,22 @@
+const os = require("os");
+const path = require("path");
+
+// const userInfo = os.userInfo();
+// console.log(userInfo);
+
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.uptime());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const extenName = path.extname("index.html");
+// console.log(extenName);
+
+const joins = path.join(__dirname + "/sumon");
+console.log(joins);
+
+// console.log(path.basename());
