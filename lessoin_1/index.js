203776b08c7ee590beda384c6f4e1eb690d2387c
mnosumon <mdnuruddin378350@gmail.com>
2025-08-10 17:05:34 +0600

    Local module practice done

---
+++ lessoin_1/index.js
@@ -0,0 +1,9 @@
+// const student = require("./students");
+// const { getName } = require("./students");
+const { getName } = require("./students");
+
+// console.log(student.getName());
+// console.log(student.getAge());
+// console.log(student.getCgpa());
+// console.log(student.name);
+console.log(getName());
---
+++ lessoin_1/students.js
@@ -0,0 +1,26 @@
+// const getName = () => {
+//   return "Hi Nuruddin";
+// };
+exports.getName = () => {
+  return "Hi Nuruddin";
+};
+const getAge = () => {
+  return "27";
+};
+const getCgpa = () => {
+  return "3.44";
+};
+
+const name = "Sumon";
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.getCgpa = getCgpa;
+// exports.name = name;
+
+// module.exports = {
+//   getName,
+//   getAge,
+//   getCgpa,
+//   name,
+// };
