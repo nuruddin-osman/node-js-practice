e481f2ceac1e6e96adc9fb234f6928597cbd2ae1
mnosumon <mdnuruddin378350@gmail.com>
2025-08-12 23:54:00 +0600

    how to make post request with josn or from data

--- lesson_8/index.js
+++ lesson_8/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>This is an Home page</h1>");
@@ -9,6 +13,29 @@ app.get("/user", (req, res) => {
   // const name = req.query.name;
   const { id, name } = req.query;
   res.send(`<h1>User id: ${id}</h1><h1>User name: ${name}</h1>`);
+});
+app.get("/route-perametter/userId/:id/userName/:name", (req, res) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  res.send(`<h1>User id: ${id}</h1><h1>User name: ${name}</h1>`);
+});
+app.get("/header", (req, res) => {
+  const id = req.header("id");
+  const name = req.header("name");
+  res.send(`<h1>User id: ${id}</h1><h1>User name: ${name}</h1>`);
+});
+app.get("/body-parser", (req, res) => {
+  // const name = req.body.name;
+  // const email = req.body.email;
+  // const phone = req.body.phone;
+  // const subject = req.body.subject;
+  const { name, email, phone, subject } = req.body;
+  res.send(`
+    name: ${name} <br/>
+    email: ${email} <br/>
+    phone: ${phone} <br/>
+    subject: ${subject}
+  `);
 });
 
 app.listen(3000, () => {
