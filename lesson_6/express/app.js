0b259864a7ed019245d79814eca540a61025db4b
mnosumon <mdnuruddin378350@gmail.com>
2025-08-12 13:36:39 +0600

    http methods and postman = get, post, delete, put

--- lesson_6/express/app.js
+++ lesson_6/express/app.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is an Get request in Home page");
+  res.end();
+});
+app.post("/", (req, res) => {
+  res.send("This is an Post request in Home page");
+  res.end();
+});
+app.put("/", (req, res) => {
+  res.send("This is an Put request in Home page");
+  res.end();
+});
+app.delete("/", (req, res) => {
+  res.send("This is an Delete request in Home page");
+  res.end();
+});
+
 module.exports = app;
