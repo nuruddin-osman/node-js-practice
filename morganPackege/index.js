3f0129160fd8154517f6db78f615ec4619117c00
mnosumon <mdnuruddin378350@gmail.com>
2025-09-02 17:00:11 +0600

    chalk npm package

--- morganPackege/index.js
+++ morganPackege/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const chalk = require("chalk");
 
 const app = express();
 app.use(morgan("dev"));
+
+const error = chalk.bold.red;
+const warning = chalk.hex("#FFA500").bold;
+
+console.log(error("this is an error"));
+console.log(warning("this is an warning"));
 
 const PORT = 3990;
 
@@ -14,5 +21,11 @@ app.post("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`this server is running at http://localhost:${PORT}`);
+  console.log(
+    chalk.bgRed.bold.rgb(
+      255,
+      136,
+      0
+    )(`this server is running at http://localhost:${PORT}`)
+  );
 });
