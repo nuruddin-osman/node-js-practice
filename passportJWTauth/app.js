d4370e1a1b36d22d97be479ca9a25d49becd1584
mnosumon <mdnuruddin378350@gmail.com>
2025-09-09 10:39:22 +0600

    Routes setup done

--- passportJWTauth/app.js
+++ passportJWTauth/app.js
@@ -2,4 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.post("/register", (req, res) => {
+  res.send("register");
+});
+app.post("/login", (req, res) => {
+  res.send("register");
+});
+app.get("/profile", (req, res) => {
+  res.send("This is an profile");
+});
 module.exports = app;
--- passportJWTauth/index.js
+++ passportJWTauth/index.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.send("This is an home page");
 });
 
+app.use((req, res, next) => {
+  res.status(404).send("route is not found");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send("Something broke!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
