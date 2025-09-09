f22d6a8f617e5555622a0974e4dd182a243efa1c
mnosumon <mdnuruddin378350@gmail.com>
2025-09-09 12:07:36 +0600

    setup mongoDb database and create mongoose models and schema

--- passportJWTauth/app.js
+++ passportJWTauth/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("./config/database");
 
 app.post("/register", (req, res) => {
   res.send("register");
