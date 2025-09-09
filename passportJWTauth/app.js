c1fc9ef83c771e996da2d7a4c03155bf59c0fea3
mnosumon <mdnuruddin378350@gmail.com>
2025-09-09 15:19:51 +0600

    user login success and token generate successfull

--- passportJWTauth/app.js
+++ passportJWTauth/app.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 require("./config/database");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
 app.use(cors());
@@ -27,7 +28,7 @@ app.post("/register", async (req, res) => {
       await newUser
         .save()
         .then(() => {
-          return res.send({
+          return res.status(201).send({
             status: true,
             message: "user create success",
             data: newUser,
@@ -44,8 +45,45 @@ app.post("/register", async (req, res) => {
     res.status(500).send({ message: "Internal server error" });
   }
 });
-app.post("/login", (req, res) => {
-  res.send("register");
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(404).send({
+        message: "User is  not found",
+        status: false,
+      });
+    }
+    const passwordMatchCheck = await bcrypt.compare(password, user.password);
+    if (!passwordMatchCheck) {
+      return res.status(404).send({
+        message: "Password do not match",
+        status: false,
+      });
+    }
+
+    const payload = {
+      id: user._id,
+      username: user.username,
+    };
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "2d",
+    });
+    const userWithoutPassword = { ...user.toObject() };
+    delete userWithoutPassword.password;
+    console.log(userWithoutPassword);
+    res.status(200).send({
+      message: "Login success",
+      data: {
+        user: userWithoutPassword,
+        token: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 app.get("/profile", (req, res) => {
   res.send("This is an profile");
